6da8df35d77d0940275ce6e0f0a0e70ff8ca6697
Crafter-deo <adesolaadeoluwa@gmail.com>
2020-05-09 01:01:21 +0100

    third commit

--- main.py
+++ main.py
@@ -18,21 +18,45 @@ def page_2(username):
                           for i in range(10))
         print(acc_num)
         customer_details = open("customer.txt", "a")
-        customer_details.write(acc_name, opening_balance, acc_type, acc_email, acc_num)
+        customer_details.write(acc_num + ',' + acc_name + ',' + str(opening_balance) + ',' + acc_type + ',' +
+                               acc_email + '\n')
+        customer_details.close()
         user_session = open("user_session.txt", "a")
         user_session.write(f"{username} created an account")
         user_session.close()
         print("Account created successfully")
         print("")
-        page_2()
+        page_2(username)
 
     elif action2 == "2":
-        input("Enter your account number: ")
-        customer_details = open("customer.txt", "a+")
-        customer_details.write(f"{time_stamp} {username} checked account details")
+        while True:
+            account_no = str(input("Enter account number: "))
+            customer_details = open("customer.txt", "r")
+            user_session = open("user session.txt", "a+")
+            for row in customer_details:
+                list1 = row.split(",")
+                acc_name = str(list1[0])
+                opening_balance = str(list1[1])
+                acc_type = str(list1[2])
+                acc_email = str(list1[3])
+                acc_num = str(list1[4])
+                lastchar = len(acc_num) - 1
+                acc_number = acc_num[0:lastchar]
+                if str(account_no) == acc_number:
+                    print(f"Account details are as follows:\n {acc_name}\n "
+                          f"{opening_balance}\n {acc_type}\n {acc_email}")
+                    customer_details.close()
+                    user_session.write(f"{time_stamp} {username} checked account details")
+                    user_session.close()
+                    break
+
+            else:
+                print("Account number incorrect")
+            break
 
     elif action2 == "3":
-        os.remove("user session.txt")
+        os.unlink("user session.txt")
+        print("You are now logged out")
         login_page()
 
     else:
@@ -41,24 +65,27 @@ def page_2(username):
 
 def login_page():
     print("Please enter your details to login")
-    username1 = input("Enter your username: ")
-    password1 = input("Enter your password: ")
 
-    login_details = open("staff.txt", "r")
-    found_details = False
+    unauthorised = True
 
-    while not found_details:
-        staff_list = login_details.read()
-        new_list = staff_list.strip('\n')
-        new_list2 = new_list.split(',')
-        if username1 == new_list2[0] and password1 == new_list2[1]:
-            found_details = True
-            print("Hello ", new_list2[3])
-            user_session = open("user session.txt", "w")
-            user_session.write(f"{time_stamp}: {username1} logged in")
-            user_session.close()
-            print("")
-            page_2()
+    while unauthorised:
+        username1 = str(input("Enter your username: "))
+        password1 = str(input("Enter your password: "))
+
+        login_details = open("staff.txt", "r")
+        for row in login_details:
+            staff_list = row.split(",")
+            username = str(staff_list[0])
+            password = str(staff_list[1])
+
+            if username1 == username and password1 == password:
+                print("Hello ", staff_list[3])
+                user_session = open("user session.txt", "w")
+                user_session.write(f"{time_stamp}: {username1} logged in")
+                user_session.close()
+                unauthorised = False
+                print("")
+                page_2(username1)
 
         else:
             print("Incorrect details. Please try again")
